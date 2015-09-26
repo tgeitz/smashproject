545a1fa915948aa0bcf4b63c02f375a6a70ebfc3
Tom Geitz <tgeitz@gmail.com>
2015-09-26 15:54:15 -0500

    Fixed CSS loading

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,13 +4,15 @@
     <meta charset="utf-8">
     <title>Dashboard</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css"/>
-    <link rel="stylesheet" href="public/css/bootstrap.css" />
+    <link rel="stylesheet" href="{{ URL::asset('css/app.css') }}" />
 </head>
 
 <body>
 <div class="container">
-    {{--@include('partials.navigationbar')--}}
-    @yield('content')
+    @include('partials.navigationbar')
+    <div class="content">
+        @yield('content')
+    </div>
 </div>
 @yield('footer')
 </body>
